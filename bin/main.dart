4f7d20c9c856407075448ef6cc068da16a053288
Felipe de Lima <f3lip303@alu.ufc.br>
2018-12-10 10:19:37 -0300

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,26 @@
+import 'package:alo_dart/alo_dart.dart' as alo_dart;
+
+main(List<String> arguments) {
+  int balde = 3;
+  double v ;
+
+  String minhaVar = "Mocambique";
+  num idade = 21;
+  idade = 21.5;
+  num altura = 1.70;
+
+  bool verdade = true;
+  print(verdade);
+
+  print(idade);
+  print(altura);
+
+//  var
+//  String
+//  Number
+//  Booleans
+//  List
+//  Maps
+//  Classes
+
+}
---
+++ test/alo_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:alo_dart/alo_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
